d35d4488f7b1893dfde3dc700297dc2a37c6c507
brendancol <brendan@makepath.com>
2021-09-16 10:57:07 -0400

    added setup fiels

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='numba-rtx',
+    version='0.0.1',
+    packages=['rtx'],
+    license='MIT',
+    description='',
+    author='makepath',
+    url='https://github.com/makepath/numba-rtx',
+    install_requires=[
+        'numba',
+    ],
+)
