fd74204825abbedcd4d7c85eb9040b48b4d00a94
brendancol <brendan@makepath.com>
2025-12-22 10:17:28 -0500

    added example files

---
+++ examples/playground.py
@@ -0,0 +1,122 @@
+from datashader.transfer_functions import shade
+from datashader.transfer_functions import stack
+from datashader.transfer_functions import dynspread
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+import cupy
+
+import os, sys
+#Add the parent folder to python search space
+sys.path.append(os.path.abspath(os.getcwd())+"/..")
+sys.path.append(os.path.abspath(os.getcwd()))
+
+from raytrace import hillshade_gpu
+from raytrace import viewshed_gpu
+
+from rtxpy import RTX
+
+import xarray as xr;
+
+from raytrace.hillshade import getSunDir
+#getSunDir(angle_altitude, azimuth)
+
+#terrain = xr.open_dataarray("olympic_national_park.nc")
+#terrain.data = terrain.data * 0.025 #scale down
+terrain = xr.open_dataarray("crater_lake_national_park.nc")
+
+terrain = terrain[::2, ::2]
+
+terrain.data = terrain.data * 0.2 #scale down
+
+azimuth = 225
+
+
+def debug(x,y):
+    rays = cupy.float32([x,y,10000,0,0,0,-1,np.inf])
+    hits = cupy.float32([0,0,0,0])
+    optix = RTX()
+    res = optix.trace(rays, hits, 1)
+    norm = cupy.asnumpy(hits[1:])
+    sun = getSunDir(25, azimuth)
+
+    return res
+
+def onclick(event):
+    """
+    Simple click handler for live adjustment of the viewshed origin when running matplotlib live session
+    """
+    ix, iy = event.xdata, event.ydata
+    print ('x = {}, y = {}'.format(ix, iy))
+
+    nix = ix/terrain.shape[1]
+    niy = iy/terrain.shape[0]
+
+    x_coords = terrain.indexes.get('x').values
+    y_coords = terrain.indexes.get('y').values
+    rangex = x_coords.max() - x_coords.min()
+    rangey = y_coords.max() - y_coords.min()
+
+    global vsw, vsh
+    vsw = x_coords.min() + nix*rangex
+    vsh = y_coords.max() - niy*rangey
+
+    #debug(ix, iy)
+    return None
+
+def test():
+    runs = 360
+    H,W = terrain.data.shape
+    if isinstance(terrain.data, np.ndarray):
+        terrain.data = cupy.array(terrain.data)
+
+    fig = plt.figure()
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    colors = np.uint8(np.zeros((H,W,3)))
+    imgplot = plt.imshow(colors)
+
+    x_coords = terrain.indexes.get('x').values
+    y_coords = terrain.indexes.get('y').values
+    midx = x_coords.min() + (x_coords.max() - x_coords.min()) / 2
+    midy = y_coords.min() + (y_coords.max() - y_coords.min()) / 2
+
+    global vsw, vsh
+    global azimuth
+    vsw = midx
+    vsh = midy
+
+    import time
+    for i in range(runs):
+        print("Begin Frame ", i)
+        azimuth = azimuth + 5
+        if (azimuth > 360):
+            azimuth -= 360
+
+        beforeRT = time.time()
+        hs = hillshade_gpu(terrain, shadows=True, azimuth=azimuth, angle_altitude=25)
+        vs = viewshed_gpu(terrain, x=vsw, y=vsh, observer_elev=0.01)
+        afterRT = time.time()
+        print("  RT took ", afterRT-beforeRT)
+
+        img = np.uint8(hs.data.get()*225)
+
+        withViewshed = True#False
+        if withViewshed:
+            visBuf = np.uint8(vs.data.get() > 0) * 255
+            view = np.maximum(visBuf,img)
+        else:
+            view = img
+        colors[:,:,0] = view
+        colors[:,:,1] = img
+        colors[:,:,2] = img
+        imgplot.set_data(colors)
+        plt.pause(0.001)
+
+    plt.show()
+
+    return
+
+res = test()
+
+print("Done")
